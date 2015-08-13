bb7abf5c006ad2b928b71d67556038617b3ba7fb
Bogdan Rotund <bogdanr10@yahoo.com>
2015-08-13 23:40:35 +0300

    Added TapeEquilibrium

---
+++ TapeEquilibrium/TapeEquilibrium.Test/TapeEquilibriumTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TapeEquilibrium.Test
+{
+    [TestClass]
+    public class TapeEquilibriumTest
+    {
+        [TestMethod]
+        public void SampleInput()
+        {
+            // Arrange
+            int[] A = { 3, 1, 2, 4, 3 };
+
+            // Act
+            var result = TapeEquilibrium.Solution(A);
+
+            // Assert
+            Assert.AreEqual(result, 1);
+        }
+    }
+}
---
+++ TapeEquilibrium/TapeEquilibrium/TapeEquilibrium.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TapeEquilibrium
+{
+    public class TapeEquilibrium
+    {
+        public static int Solution(int[] A)
+        {
+            return 0;
+        }
+    }
+}
