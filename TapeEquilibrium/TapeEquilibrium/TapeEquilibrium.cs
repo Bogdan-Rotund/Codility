50d5ed565993250c686c290eed72e5944df80e74
Bogdan Rotund <bogdanr10@yahoo.com>
2015-08-17 09:17:57 +0300

    Added implementation to TapeEquilibrium

--- TapeEquilibrium/TapeEquilibrium/TapeEquilibrium.cs
+++ TapeEquilibrium/TapeEquilibrium/TapeEquilibrium.cs
@@ -10,7 +10,26 @@ namespace TapeEquilibrium
     {
         public static int Solution(int[] A)
         {
-            return 0;
+            var B = new int[A.Length];
+            var sum = 0;
+            var min = int.MaxValue;
+
+            for (var i = A.Length - 1; i >= 0; i--)
+            {
+                sum += A[i];
+                B[i] = sum;
+            }
+
+            sum = 0;
+            for (var j = 0; j < A.Length - 1; j++)
+            {
+                sum += A[j];
+                var diff = Math.Abs(sum - B[j + 1]);
+                if (diff < min)
+                    min = diff;
+            }
+
+            return min;
         }
     }
 }
