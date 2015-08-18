69c0a6bc37263f5cdb03127dd7e960a91376063a
Bogdan Rotund <bogdanr10@yahoo.com>
2015-08-18 09:38:45 +0300

    Implemented PermMissingElem

---
+++ PermMissingElem/PermMissingElem.Test/PermMissingElemTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PermMissingElem.Test
+{
+    [TestClass]
+    public class PermMissingElemTest
+    {
+        [TestMethod]
+        public void Sample()
+        {
+            // Arrange
+            int[] A = { 2, 3, 1, 5 };
+
+            // Act
+            var result = PermMissingElem.Solution(A);
+
+            // Assert
+            Assert.AreEqual(result, 4);
+        }
+
+        [TestMethod]
+        public void HighNumbers()
+        {
+            // Arrange
+            var range1 = Enumerable.Range(1, 50000);
+            var range2 = Enumerable.Range(50002, 50000);
+            var A = range1.Concat(range2).ToArray();
+
+            // Act
+            var result = PermMissingElem.Solution(A);
+
+            // Asert
+            Assert.AreEqual(result, 50001);
+        }
+    }
+}
---
+++ PermMissingElem/PermMissingElem/PermMissingElem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermMissingElem
+{
+    public class PermMissingElem
+    {
+        public static int Solution(int[] A)
+        {
+            long n = A.Length + 1;
+            long sum = (n * (n + 1)) / 2;
+            
+            for (int i = 0; i < A.Length; i++)
+            {
+                sum -= A[i];
+            }
+
+            return (int)sum;
+        }
+    }
+}
