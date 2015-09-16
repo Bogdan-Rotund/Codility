278727f0a3203646999400cde06d5a5f202489a2
Bogdan Rotund <bogdan.rotund@nearform.com>
2015-09-16 18:13:35 +0300

    Implementation for FrogRiverOne

---
+++ FrogRiverOne/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function solution(X, A) {
+    var B = [];
+    var sum = (X * (X + 1)) / 2;
+
+    for (var i = 0; i < A.length; i++) {
+        if (!B[A[i]]) {
+            sum -= A[i];
+            if (sum == 0) {
+                return i;
+            }
+            B[A[i]] = true;
+        }
+    }
+
+    return -1;
+}
