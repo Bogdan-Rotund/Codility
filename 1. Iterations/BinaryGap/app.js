3952631d1804c69884399df7fa5c4b1d11dd88e2
Bogdan Rotund <bogdan.rotund@nearform.com>
2016-03-29 14:41:47 +0300

    Solved BinaryGap

--- 1. Iterations/BinaryGap/app.js
+++ 1. Iterations/BinaryGap/app.js
@@ -1,5 +1,30 @@
 "use strict";
 
-module.exports = {
+function solution (N) {
+  let maxBinaryGap = 0;
+  let currentBinaryGap = 0;
+  let binaryGapStarted = false;
   
-};
+  do {
+    let remainder = N % 2;
+    N = Math.floor(N / 2);
+    
+    if (remainder === 1) {
+      if (binaryGapStarted) {
+        if (currentBinaryGap > maxBinaryGap) {
+          maxBinaryGap = currentBinaryGap;
+        }
+        currentBinaryGap = 0;
+      } else {
+        binaryGapStarted = true;
+      }
+    } else if (remainder === 0) {
+      if (binaryGapStarted) {
+        currentBinaryGap++;
+      }
+    }
+    
+  } while (N > 0);
+  
+  return maxBinaryGap;
+}
