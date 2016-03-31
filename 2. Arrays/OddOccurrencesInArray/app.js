77e37ed2e4c5814fb3313d2c482e224204c582a3
Bogdan Rotund <bogdan.rotund@nearform.com>
2016-03-31 10:46:43 +0300

    Added solution for OddOccurrencesInArray

--- 2. Arrays/OddOccurrencesInArray/app.js
+++ 2. Arrays/OddOccurrencesInArray/app.js
@@ -1,1 +1,13 @@
 "use strict";
+
+function solution (A) {
+  let oddOccurence = 0;
+  
+  for (let i = 0; i < A.length; i++) {
+    oddOccurence = oddOccurence ^ A[i];
+  }
+  
+  return oddOccurence;
+}
+
+console.log(solution([9, 3, 9, 3, 9, 7, 9]));
